1ff74a46292c9d062a9c1faef595b5c59f65a33f
SIMIAN WANG <you@example.com>
2017-11-16 02:24:24 +1100

    small thing

---
+++ src/TestIO.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class TestIO {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Enter integer a: ");
+        int a = in.nextInt();
+        System.out.print("Enter string b: ");
+        String b = in.next();
+        System.out.println("a: " + a + ", b: "+ b);
+    }
+}
